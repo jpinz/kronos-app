6bfa7fb77d77e1ea9da8c925213ee5cb13ef76e2
Julian <julian.pinzer@gmail.com>
2020-08-25 01:33:43 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="home">
-    <h1>This is the home page.</h1>
-    <router-link to="/create">Begin the website creation flow</router-link>
+  <div class="container">
+    <br />
+    <br />
+    <div class="notification">
+      <h1>This is the home page.</h1>
+      <router-link to="/create">Begin the website creation flow</router-link>
+    </div>
   </div>
 </template>
